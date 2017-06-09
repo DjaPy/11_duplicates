b8ffbd24bd2a273e40d99ffbd562031a40a88c09
Ivanov Sergey <djapy@yandex.ru>
2017-06-09 15:40:04 +0300

    create full script

--- duplicates.py
+++ duplicates.py
@@ -21,31 +21,26 @@ def get_size_and_name(files_list):
 
 def search_duplicates(list_size_and_name):
     duplicate_list = []
-
     for index,file_check in enumerate(list_size_and_name):
         for file in list_size_and_name[(index+1):]:
             if file_check == file:
                 duplicate_list.append(file_check)
-
     return(duplicate_list)
 
 
-
-
-
-# def get_full_info(duplicate_set, dict_files):
-#     list_duplicate = []
-#     print(duplicate_set)
-#     for file_info in dict_files:
-#         if file_info in duplicate_set:
-#             list_duplicate.append(file_info)
-#             print(list_duplicate)
-#     return list_duplicate
-
+def get_full_info(duplicate_list, files_list):
+    full_info_duplicate = []
+    for file in files_list:
+        for duplicate in duplicate_list:
+            if file[0] == duplicate[0] and file[1] == duplicate[1]:
+                full_info_duplicate.append(file)
+    return full_info_duplicate
 
 if __name__ == '__main__':
-    path = '/home/kento/devman'
+    path = '/path/to/folder'
     files_list = get_files_in_path(path)
-    print(get_size_and_name(files_list))
     list_size_and_name = get_size_and_name(files_list)
-    print(search_duplicates(list_size_and_name))
+    duplicate_list = search_duplicates(list_size_and_name)
+    full_info_duplicate = get_full_info(duplicate_list, files_list)
+    for full_info in full_info_duplicate:
+        print(full_info)
