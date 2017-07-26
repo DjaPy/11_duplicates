198dab2b60804ea0645dbf8c82d216f5b702dda0
Ivanov Sergey <djapy@yandex.ru>
2017-07-26 14:50:36 +0300

    Fix the issue of performance search duplicate files

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ This script searches for duplicate files. You must specify the path to search fo
 # How to use
 
 To run the script you need python 3.5 and later.
-```bash
-python duplicates.py /path/to/dir
-
-File 'name_file' duplicated on the way /path/to/name_file/
+```
+python duplicates.py /home/kento/devman/Test_programms
+Duplicate a file "README.md" size of 195kb on the path:
+| 1. /home/kento/devman/Test_programms/11_duplicate/111/11/README.md
+| 2. /home/kento/devman/Test_programms/11_duplicate/README.md
+Duplicate a file "duplicates.py" size of 1251kb on the path:
+| 1. /home/kento/devman/Test_programms/11_duplicate/11/duplicates.py
+| 2. /home/kento/devman/Test_programms/11_duplicate/duplicates.py
+| 3. /home/kento/devman/Test_programms/11_duplicate/111/11/duplicates.py
 ```
 
 # Project Goals
--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,5 @@
 import os
 from argparse import ArgumentParser
-from operator import itemgetter
 from collections import defaultdict
 
 
@@ -24,19 +23,23 @@ def get_files_in_path(path):
 
 
 def search_duplicates(file_list):
+    duplicate_list = []
     counter_keys = defaultdict(list)
     for file_info, path_file in file_list:
         counter_keys[file_info].append(path_file)
-    duplicate_list = sorted(counter_keys.items())
+    intermediate_list = sorted(counter_keys.items())
+    for full_file_info in intermediate_list:
+        if len(full_file_info[1]) > 1:
+            duplicate_list.append(full_file_info)
     return duplicate_list
 
 
 def display_the_result(duplicate_list):
     for info_about_file in duplicate_list:
-        duplicate_file = 'File {} duplicated on the way:'.format(info_about_file[0][1])
+        duplicate_file = 'Duplicate a file "{}" size of {}kb on the path:'.format(info_about_file[0][1], info_about_file[0][0])
         print(duplicate_file)
-        for number, pathes_to_duplicate_files in enumerate(info_about_file[1]):
-            info_about_path = '{}. {}'.format(number, pathes_to_duplicate_files)
+        for number, pathes_to_duplicate_files in enumerate(info_about_file[1], start=1):
+            info_about_path = '| {}. {}'.format(number, pathes_to_duplicate_files)
             print(info_about_path)
 
 
@@ -44,6 +47,5 @@ if __name__ == '__main__':
     option = get_path()
     path = option.path
     file_list = get_files_in_path(path)
-    print(search_duplicates(file_list))
     duplicate_list = search_duplicates(file_list)
-    print(display_the_result(duplicate_list))
+    display_the_result(duplicate_list)
